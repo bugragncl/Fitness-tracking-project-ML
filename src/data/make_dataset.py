ea127a0429998b3b1048e146b37b2242ea9b354a
bugragncl <bgrgncl@gmail.com>
2025-08-07 11:40:00 +0300

    part 1 finish make dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -47,32 +47,149 @@ for i in files:
     particpant = i.split("-")[0].replace(data_path, "")
     label = i.split("-")[1]
     category = i.split("-")[2].rstrip("123").rstrip("MetaWear_2019")
-    print(category)
 
+    df = pd.read_csv(i)
+
+    df["particpant"] = particpant
+    df["label"] = label  # we added these extra columns to df
+    df["category"] = category
+
+    if "Accelerometer" in i:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+
+    if "Gyroscope" in i:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+
+acc_df[acc_df["set"] == 10]
 
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
+pd.to_datetime(df["epoch (ms)"], unit="ms")
+pd.to_datetime(df["time (01:00)"])
 
+acc_df.index
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
+
+acc_df
+gyr_df
 
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
 
 
+def read_data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+    acc_set = 1
+    gyr_set = 1
+
+    for i in files:
+        particpant = i.split("-")[0].replace(data_path, "")
+        label = i.split("-")[1]
+        category = i.split("-")[2].rstrip("123").rstrip("MetaWear_2019")
+
+        df = pd.read_csv(i)
+
+        df["particpant"] = particpant
+        df["label"] = label  # we added these extra columns to df
+        df["category"] = category
+
+        if "Accelerometer" in i:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+
+        if "Gyroscope" in i:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+
+    return acc_df, gyr_df
+
+
+acc_df, gyr_df = read_data_from_files(files)
+acc_df
+gyr_df
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
+acc_df.iloc[:, :3]
 
+data_merged = pd.concat([acc_df.iloc[:, :3], gyr_df], axis=1)
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "participant",
+    "label",
+    "category",
+    "set",
+]
+data_merged
 
+gyr_df[gyr_df["set"] == 30]
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
 # --------------------------------------------------------------
 
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participant": "last",
+    "set": "last",
+}
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
 
+data_resampled = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
+data_resampled["set"] = data_resampled["set"].astype("int")
+
+data_resampled
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+data_resampled.to_pickle("../../data/interim/0.1_data_processed.pkl")
+# csv olarak değil pickle olarak yaptık çünkü timestamps ile çalışırken
+# dosyayı export ettikten sonra tekrar okuduğumuzda aynı şekilde geliyor
+# ayarlama gerektirmiyor
